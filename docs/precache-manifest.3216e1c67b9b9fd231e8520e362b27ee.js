3f0f40e621b296522d117e235d9827974518fcb6
Oscar Pulgaron <o.ahumadap@gmail.com>
2020-08-01 18:59:18 -0500

    gif-expert-app-docs-react

---
+++ docs/precache-manifest.3216e1c67b9b9fd231e8520e362b27ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f024dcd5a22279f8f92bea961b90e4c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a35842d9440090694c8e",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "3a1c1cdf8e773509c287",
+    "url": "/static/css/main.a425696c.chunk.css"
+  },
+  {
+    "revision": "a35842d9440090694c8e",
+    "url": "/static/js/2.5c8e4ae5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5c8e4ae5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a1c1cdf8e773509c287",
+    "url": "/static/js/main.ebafa7ae.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
